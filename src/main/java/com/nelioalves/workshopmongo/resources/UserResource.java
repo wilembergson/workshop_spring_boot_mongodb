14b2c2f7e1826273d7f74f0975ee9535d4c17e4b
wilembergson <wilembergson@yahoo.com>
2020-08-06 18:24:12 -0300

    Obtendo usuario pelo id

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -1,12 +1,15 @@
 package com.nelioalves.workshopmongo.resources;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.workshopmongo.damain.User;
@@ -25,5 +28,11 @@ public class UserResource {
 		List<User> list = service.fingAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 }
